1efda8c90a95f158b4f427f6e8bfa5ea9890a9e0
hungpv <phanhung2702@gmail.com>
2022-12-22 15:10:42 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#Variable
+name = 'hungpv'
+age = 22
+print('I am ' + name + ' ' +  str(age))
+print('Could check data type by type: ' + str(type(name)))
+# Multiple assignment:
+# name, age = 'hungpv', 22
+
+# String methods
+name = 'hung'
+print(len(name))
+print(name.find('un'))
+print(name.isdigit())
+print(name.isalpha())
+print(name.count('h'))
+print(name * 3)
